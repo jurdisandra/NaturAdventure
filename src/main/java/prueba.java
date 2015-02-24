151eb5c4128ffa547e13a9d3c1936889569d3ccb
Sandra <jurdisandra@gmail.com>
2015-02-24 18:55:55 +0100

    pruebas3

--- src/main/java/prueba.java
+++ src/main/java/prueba.java
@@ -2,4 +2,7 @@
  * Created by Sandra on 24/02/15.
  */
 public class prueba {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
 }
